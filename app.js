a3075cf6785d85bad73d98fee64c2e76da001311
RESEGO MOTLHASI <remotlhjhb024@student.wethinkcode.co.za>
2025-05-30 16:48:37 +0200

    Added the typescript for generating the poem

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
+let poemForm = document.querySelector("#poem-generator");
 
+function generatePoem(event) {
+  event.preventDefault();
+
+  new Typewriter("#poem", {
+    strings:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque sunt impedit aliquam culpa numquam voluptatem labore asperiores. Eligendi magnam minima rerum voluptatem eius, beatae eum dolor voluptatum repudiandae sint harum!",
+    autoStart: true,
+    delay: 1,
+    cursor: "",
+  });
+}
+poemForm.addEventListener("submit", generatePoem);
